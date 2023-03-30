c03fe8b0b904b66b552660a5f4fd53c191d744a7
Pavel Guranda <pguranda@3-C-2.42heilbronn.de>
2023-03-30 13:48:41 +0200

    upd

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -13,9 +13,10 @@ BitcoinExchange::BitcoinExchange( const BitcoinExchange & src )
 	*this = src;
 }
 
-BitcoinExchange::BitcoinExchange(std::string inputFile) : _inputFile(inputFile)
+BitcoinExchange::BitcoinExchange(std::string const &inputFile) : _inputFile(inputFile)
 {
 	this->checkFiles();
+	this->fillDataMap();
 }
 /*
 ** -------------------------------- DESTRUCTOR --------------------------------
@@ -32,39 +33,27 @@ BitcoinExchange::~BitcoinExchange()
 
 BitcoinExchange &				BitcoinExchange::operator=( BitcoinExchange const & rhs )
 {
-	(void)rhs;
+	this->_inputFile = rhs._inputFile;
 	return *this;
 }
 
 std::ostream &					operator<<( std::ostream & o, BitcoinExchange & i )
 {
-	o << "The input map = " << i.getInput();
+	o << "The input map = " << i.getData();
 	return o;
 }
 
-//Priniting the input map
 std::ostream& operator<<(std::ostream& os, const std::map<Date, float>& m)
 {
-    for (std::map<Date, float>::const_iterator it = m.begin(); it != m.end(); ++it) {
+    for (std::map<Date, float>::const_iterator it = m.begin(); it != m.end(); ++it) 
+	{
         os << it->first.joinedDate << " | " << it->second << std::endl;
     }
     return os;
 }
 
-
-
 bool 			operator<(const Date& lhs, const Date& rhs)
 {
-
-
-    // if (lhs.year != rhs.year) {
-    //     return lhs.year < rhs.year;
-    // }
-    // if (lhs.month != rhs.month) {
-    //     return lhs.month < rhs.month;
-    // }
-		// return lhs.index < rhs.index;
-
 	if (lhs.year != rhs.year) {
 		return lhs.year < rhs.year;
 	}
@@ -77,9 +66,7 @@ bool 			operator<(const Date& lhs, const Date& rhs)
 /*
 ** --------------------------------- METHODS ----------------------------------
 */
-//filling _data and _input maps with the input from input and data files
-
-Date	BitcoinExchange::fillMapDate(std::string &line, char  delimeter)
+Date	BitcoinExchange::fillMapDate(std::string &line, char delimeter)
 {
 	Date date;
 	date.joinedDate = line.substr(0, line.find(delimeter));
@@ -98,8 +85,6 @@ Date	BitcoinExchange::fillMapDate(std::string &line, char  delimeter)
 	{
 		throw BitcoinExchange::BadInput(line);
 	}
-	// if (date.year <= 0 || date.month <= 0 || date.day <= 0 || date.joinedDate.empty()) //close stream
-	// 	exit (1);
 	return date;
 }
 
@@ -118,52 +103,6 @@ float		BitcoinExchange::fillMapValue(std::string &line, char  delimeter)
 	return value;
 }
 
-
-void	BitcoinExchange::fillInputMap()
-{
-	std::ifstream *fsInput = new std::ifstream(this->getInputFile());
-	setfsInput(fsInput);
-	std::string line;
-	float value;
-	float *value_new;
-	Date date;
-	Date *date_new;
-	std::pair <Date, float> pair;
-	char delimeter = '|';
-	if (getfsInput()->is_open() && getfsInput()->good())
-	{
-		while (getline(*getfsInput(), line))//TODO more parsing
-		{
-			// std::cout << line << std::endl;
-			if (!line.find(delimeter))
-				exit (1);
-			if (line.find('-') == std::string::npos)
-				continue ;
-			try
-			{
-				date = this->fillMapDate(line, delimeter);
-				value = this->fillMapValue(line, delimeter);
-			}
-			catch (const std::exception& e)
-			{
-				std::cerr << e.what() << '\n';
-			}
-			
-			date_new = new Date(date);
-			value_new = new float(value);
-			pair = std::make_pair(*date_new, *value_new);
-			this->setInput(pair);
-		}
-	}
-	else
-	{
-		std::cerr << "Error\nUnable to open the file" << std::endl;
-		exit(1);
-	}
-
-	// delete fsInput;
-}
-//Funciton which opens the input file and reads it line by line
 void BitcoinExchange::performConversion()
 {
 	Date			date;
@@ -172,7 +111,8 @@ void BitcoinExchange::performConversion()
 	std::ifstream	fsInput(this->getInputFile());
 	if (fsInput.is_open() && fsInput.good())
 	{
-		while (getline(fsInput, line))//TODO more parsing
+		fsInput >> std::ws;
+		while (getline(fsInput, line))
 		{
 			try
 			{
@@ -195,31 +135,34 @@ void BitcoinExchange::performConversion()
 			}
 			std::cout << date.joinedDate << "=>" << value << " = " << value * (--getData().upper_bound(date))->second << std::endl;
 		}
-
+		fsInput.close();
+	}
+	else
+	{
+		std::cerr << "Error\nUnable to open the input file" << std::endl;
+		fsInput.close();
+		exit(1);
 	}
 }
-//Remove the malloc
+
 void	BitcoinExchange::fillDataMap()
 {
 	std::ifstream			fsData("data.csv");
 	std::string				line;
 	float					value;
-	// float					*value_new;
 	Date					date;
-	// Date					*date_new;
 	char 					delimeter = ',';
 	std::pair <Date, float> pair;
 	if (fsData.is_open() && fsData.good())
 	{
-		while (getline(fsData, line))//TODO more parsing
+		while (getline(fsData, line))
 		{
-			// std::cout << line << std::endl;
-			if (!line.find(delimeter))
-				exit (1);
-			if (line.find('-') == std::string::npos)
-				continue ;
+			if (line.empty())
+				continue;
 			try
 			{
+				if (line.find('-') == std::string::npos)
+					continue;
 				date = this->fillMapDate(line, delimeter);
 				value = this->fillMapValue(line, delimeter);
 			}
@@ -227,84 +170,19 @@ void	BitcoinExchange::fillDataMap()
 			{
 				std::cerr << e.what() << '\n';
 			}
-			// date_new = new Date(date);
-			// value_new = new float(value);
 			pair = std::make_pair(date, value);
 			this->setData(pair);
 		}
+		fsData.close();
 	}
 	else
 	{
 		std::cerr << "Error\nUnable to open the data file" << std::endl;
+		fsData.close();
 		exit(1);
 	}
 }
 
-// Function which takes the date from _input, finds the closest date in _data and returns the value
-float		BitcoinExchange::findClosestDate(Date const &date)
-{
-	std::map<Date, float>::iterator it;
-
-	it = this->getData().lower_bound(date);
-	std::cout << " the closest: " << date.joinedDate << std::endl;
-	if (it == this->getData().end())
-	{
-		std::cerr << "Error\nNo data for the given date" << std::endl;
-		exit(1);
-	}
-	return it->second;
-}
-//Introducing find-like function that will search the map for the given date
-std::map<Date, float>::iterator		BitcoinExchange::search_map(Date const &date_to_find)
-{
-	std::map<Date, float>::iterator it;
-	it = this->getData().begin();
-	while (it != this->getData().end())
-	{
-		if (it->first.year == date_to_find.year && it->first.month == date_to_find.month && it->first.day == date_to_find.day)
-		{
-			return it;
-		}
-		it++;
-	}
-	return this->getData().end();
-}
-// Takes the date from _input finds the corresponding date in _data and returns the multiplication result
-float		BitcoinExchange::findDate(Date const &date)
-{
-	std::map<Date, float>::iterator it;
-	std::cout << "Date to find: " << date.joinedDate << std::endl;
-	it = search_map(date);
-	std::cout << "Date found: " << it->first.joinedDate << std::endl;
-	if (it == this->getData().end())
-	{
-		this->findClosestDate(date);
-	}
-	return it->second;
-}
-
-//Multiply the values
-
-void		BitcoinExchange::multiplyValues()
-{
-	// std::cout << getInput().size() << std::endl;
-	// std:: map <Date, float>::iterator it = getInput().begin();
-	// std::cout << it->first.joinedDate << std::endl; 
-	std::map<Date, float>::iterator it;
-	std::map<Date, float>::iterator it2;
-
-	float value;
-	float result;
-	for (it = getInput().begin()++; it != getInput().end(); it++)
-	{
-		value = this->findDate(it->first);
-		std::cout << std::endl;
-		 std::cout << "value: " << value << std::endl;
-		//  std::cout << "exch_rate: " << exch_rate << std::endl;
-		result = it->second * value;
-		std::cout << it->first.joinedDate << "=>" << it->second << " = " << result << std::endl;
-	}
-}
 void	BitcoinExchange::checkFiles()
 {
 	if (this->getInputFile().empty())
@@ -319,22 +197,29 @@ void	BitcoinExchange::checkFiles()
 	if (!ifsInput.is_open())
 	{
 		std::cerr << "Error\nUnable to open the input file" << std::endl;
+		ifsInput.close();
 		exit(1);
 	}
 	ifsData.open("data.csv");
 	if (!ifsData.is_open())
 	{
 		std::cerr << "Error\nUnable to open the database file" << std::endl;
+		ifsInput.close();
+		ifsData.close();
 		exit(1);
 	}
 	if (ifsInput >> std::ws && ifsInput.peek () == EOF)
 	{
 		std::cerr << "Error\nEmpty input file" << std::endl;
+		ifsInput.close();
+		ifsData.close();
 		exit(1);
 	}
 	if (ifsData >> std::ws && ifsData.peek () == EOF)
 	{
 		std::cerr << "Error\nEmpty database file" << std::endl;
+		ifsInput.close();
+		ifsData.close();
 		exit(1);
 	}
 
@@ -373,19 +258,9 @@ void BitcoinExchange::setData(std::pair<Date, float> my_pair)
 }
 
 
-void BitcoinExchange::setInput(std::pair<Date, float> my_pair)
-{
-	this->_input.insert(my_pair);
-}
-
 std::map<Date, float> & BitcoinExchange::getData() 
 {
 	return this->_data;
-}
-
-std::map<Date, float>  & BitcoinExchange::getInput() 
-{
-	return this->_input;
 }
 
 std::string BitcoinExchange::getInputFile() const
@@ -393,23 +268,4 @@ std::string BitcoinExchange::getInputFile() const
 	return this->_inputFile;
 }
 
-std::ifstream *BitcoinExchange::getfsInput() const
-{
-	return this->fsInput;
-}
-
-std::ifstream *BitcoinExchange::getfsData() const
-{
-	return this->fsData;
-}
-
-void BitcoinExchange::setfsInput(std::ifstream *fdInput)
-{
-	this->fsInput = fdInput;
-}
-
-void BitcoinExchange::setfsData(std::ifstream *fdData)
-{
-	this->fsData = fdData;
-}
 /* ************************************************************************** */
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -3,14 +3,10 @@
 
 # include <iostream>
 # include <string>
-# include <vector>
 # include <map>
-# include <sstream>
 # include <fstream>
 # include <algorithm>
-# include <fstream>
-# include <utility>
-# include <limits>
+# include <climits>
 
 
 struct Date
@@ -28,36 +24,22 @@ class BitcoinExchange
 
 		BitcoinExchange();
 		BitcoinExchange( BitcoinExchange const & src );
-		BitcoinExchange(std::string inputFile);
+		BitcoinExchange(std::string const &inputFile);
 		~BitcoinExchange();
 
 		BitcoinExchange &						operator=( BitcoinExchange const & rhs );
 		std::string								inputFile;
 
 		std::map<Date, float> &					getData() ;
-		std::map<Date, float> &					getInput() ;
 
 		void									checkFiles();
 
 		void									setData(std::pair<Date, float> pair);
-		void									setInput(std::pair<Date, float> pair);
-		// void 								readiInputFile(std::string inputFile);
-		// void									readDataFile(std::string dataFile);
-		
-		void									setfsInput(std::ifstream *fdInput);
-		void									setfsData(std::ifstream *fdData);
-		std::ifstream*							getfsInput() const;
-		std::ifstream*							getfsData() const;
-
 		std::string								getInputFile() const;
-		void									fillInputMap();
 
 		float									fillMapValue(std::string &line, char delimeter);
 		Date 									fillMapDate(std::string &line, char  delimeter);
 		void									fillDataMap();
-		void									multiplyValues();
-		float									findDate(Date const &date);
-		float									findClosestDate(Date const &date);
 
 		void 									performConversion();
 
@@ -78,17 +60,13 @@ class BitcoinExchange
 			const char *what() const throw();
 
 		};
-
-		std::map<Date, float>::iterator		search_map(Date const &date_to_find);
 	private:
-		std::ifstream							*fsInput;
-		std::ifstream							*fsData;
 		std::map<Date, float>					_data;
-		std::map<Date, float>					_input;
 		std::string								_inputFile;
 
 };
 
 std::ostream& operator<<(std::ostream& os, const std::map<Date, float>& m);
+bool 			operator<(const Date& lhs, const Date& rhs);
 
 #endif /* ************************************************* BITCOINEXCHANGE_H */
